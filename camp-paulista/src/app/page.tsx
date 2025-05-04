2bdb418c9a27703828d8b706342a0074c6e16a9f
matheusviniciusbrito <matheus.vinicius.mvrb@gmail.com>
2025-05-04 17:15:45 -0300

    Estrutura de tabela com json feita

--- camp-paulista/src/app/page.tsx
+++ camp-paulista/src/app/page.tsx
@@ -1,10 +1,22 @@
-import { Table } from '../components/TablePrimeira'
+import grupoAfase1 from '../../public/data/grupoAfase1.json';
+import grupoBfase1 from '../../public/data/grupoBfase1.json';
+import grupoCfase1 from '../../public/data/grupoCfase1.json';
+import grupoDfase1 from '../../public/data/grupoDfase1.json';
+import { Table } from '../components/TablePrimeira';
 
 export default function Home() {
   return (
-  <>
-    <Table></Table>
-  </>
+    <div>
+      <h1>Grupo A</h1>
+      <Table grupo={grupoAfase1} />
+      <h1>Grupo B</h1>
+      <Table grupo={grupoBfase1} />
+      <h1>Grupo C</h1>
+      <Table grupo={grupoCfase1} />
+      <h1>Grupo D</h1>
+      <Table grupo={grupoDfase1} />
+    </div>
+    
   )
 
 }
--- camp-paulista/src/components/TablePrimeira/index.tsx
+++ camp-paulista/src/components/TablePrimeira/index.tsx
@@ -1,44 +1,61 @@
-import grupoAfase1 from '../../../public/data/grupoAfase1.json';
+interface Time {
+  classificacao: number;
+  time: string;
+  P: number;
+  J: number;
+  V: number;
+  E: number;
+  D: number;
+  GP: number;
+  GC: number;
+  SG: number;
+  porcentagem: string;
+  ultimosJogos: string[];
+}
 
-export function Table() {
-    console.log(grupoAfase1);
-    return (
-        <div>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Classificação</th>
-                        <th>P</th>
-                        <th>J</th>
-                        <th>V</th>
-                        <th>E</th>
-                        <th>D</th>
-                        <th>GP</th>
-                        <th>GC</th>
-                        <th>SG</th>
-                        <th>%</th>
-                        <th>ÚLT. JOGOS</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {grupoAfase1.map((time, index) => (
-                        <tr key={index}>
-                            <td>{time.classificacao}</td>
-                            <td>{time.P}</td>
-                            <td>{time.J}</td>
-                            <td>{time.V}</td>
-                            <td>{time.E}</td>
-                            <td>{time.D}</td>
-                            <td>{time.GP}</td>
-                            <td>{time.GC}</td>
-                            <td>{time.SG}</td>
-                            <td>{time.porcentagem}</td>
-                            <td>{time.ultimosJogos}</td>
-                        </tr>
-                    ))}
+interface TableProps {
+  grupo: Time[];
+}
 
-                </tbody>
-            </table>
-        </div>
-    );
+export function Table({ grupo }: TableProps) {
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Classificação</th>
+            <th>Time</th>
+            <th>P</th>
+            <th>J</th>
+            <th>V</th>
+            <th>E</th>
+            <th>D</th>
+            <th>GP</th>
+            <th>GC</th>
+            <th>SG</th>
+            <th>%</th>
+            <th>ÚLT. JOGOS</th>
+          </tr>
+        </thead>
+        <tbody>
+          {grupo.map((time, index) => (
+            <tr key={index}>
+              <td>{time.classificacao}</td>
+              <td>{time.time}</td>
+              <td>{time.P}</td>
+              <td>{time.J}</td>
+              <td>{time.V}</td>
+              <td>{time.E}</td>
+              <td>{time.D}</td>
+              <td>{time.GP}</td>
+              <td>{time.GC}</td>
+              <td>{time.SG}</td>
+              <td>{time.porcentagem}</td>
+              <td>{time.ultimosJogos.join(", ")}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
